c681c5707bd42c62452bd2123be2bc4852e7a013
Dipesh Bajgain <dipbazz@gmail.com>
2021-05-27 15:29:54 +0545

    Add address model with generic foreign key

--- generic/admin.py
+++ generic/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
-from .models import ActivityLog, Profile, Company
+from .models import ActivityLog, Address, Profile, Company
 # Register your models here.
 
 
 admin.site.register(ActivityLog)
 admin.site.register(Profile)
 admin.site.register(Company)
+admin.site.register(Address)
--- generic/models.py
+++ generic/models.py
@@ -24,12 +24,25 @@ class ActivityLog(models.Model):
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     contact = models.CharField(max_length=10)
-    address = models.CharField(max_length=100)
+    address = GenericRelation('Address')
     activity = GenericRelation(ActivityLog)
 
 
 class Company(models.Model):
     owner = models.OneToOneField(Profile, on_delete=models.CASCADE)
     contact = models.CharField(max_length=10)
-    address = models.CharField(max_length=100)
+    address = GenericRelation('Address')
     activity = GenericRelation(ActivityLog)
+
+
+class Address(models.Model):
+    city = models.CharField(max_length=30)
+    state = models.CharField(max_length=20)
+    street = models.CharField(max_length=30)
+    permanent = models.BooleanField(default=False)
+    content_type = models.ForeignKey(
+        ContentType,
+        on_delete=models.CASCADE
+    )
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
